50f41bb77a687943e8256f6d6c9b503a552e020b
charly <charlyvallejos22@gmail.com>
2019-03-12 17:30:01 -0300

    Merge origin/master
    Conflicts:
    	nbproject/private/private.xml

--- vistas/menu.php
+++ vistas/menu.php
@@ -5,7 +5,23 @@
         $vista = 'vistaPedidos';
     
     
-    echo 'hola';
+    
 
 ?>
+
+<script>
+    var CodVendedor = "<?php echo $_SESSION['Codigo_Vendedor'] ?>";  
+</script>
+<div class="nav-color">
+    <div class="nav-wrap">
+        <?php if(isset($_SESSION)){ ?>
+        <div id="lang-selector" ng-controller="loginController">
+            <a href=""  ng-click="logout()" class="active"><?php if(isset($_SESSION['Nombre_Vendedor'])) echo 'Cerrar Sesión'?></a>
+             | 
+            <a href=""><?php if(isset($_SESSION['Nombre_Vendedor'])) echo $_SESSION['Nombre_Vendedor'] ?></a>
+        </div>
+        <?php } ?>
+    </div>    
+</div> 
+
 
